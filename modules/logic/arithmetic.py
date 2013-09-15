e335da746448856e2ee930db2c37f48fc800b014
U-Ryo-Win8\ryotaro <ryotaro@Ryo-Win8.(none)>
2013-09-15 17:01:54 +0900

    Arithmetic logic / test is done.

---
+++ modules/logic/arithmetic.py
@@ -0,0 +1,10 @@
+#!coding:utf-8
+
+def summation(data,price,rate=0.0):
+    ret = {}
+    ret['mins'] = sum(map(lambda x: x['mins'], data))
+    ret['summation'] = int(price * (ret['mins'] / 60.0))
+    ret['payment'] = ret['summation'] * (1 - rate)
+    ret['subtract'] = ret['summation'] * rate
+    return ret
+
---
+++ test/logic_test/arithmetic_test.py
@@ -0,0 +1,21 @@
+# !coding:utf-8
+
+from nose.tools import with_setup, ok_, eq_
+from modules.logic.arithmetic import * 
+
+def test_summation():
+    target = []
+    target.append({'begintime':u"14:57:47",'endtime':u'18:05:32','mins':185 })
+    target.append({'begintime':u"",'endtime':u'18:11:28','mins':135 })
+    ret = summation(data=target, price=800)
+    eq_(ret['mins'],320)
+    eq_(ret['payment'], 4266)
+    eq_(ret['subtract'], 0)
+    eq_(ret['summation'], 4266)
+
+    ret = summation(data=target, price=800, rate=0.5)
+    eq_(ret['summation'], 4266)
+    eq_(ret['payment'], 2133)
+    eq_(ret['subtract'], 2133)
+    eq_(ret['mins'],320)
+    
